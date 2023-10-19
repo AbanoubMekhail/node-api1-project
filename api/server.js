2b1ee64ee6c72499e3dfd0ba84bd694d32c81de4
abanoub nagib <abanoub.nagib@hotmail.com>
2023-10-19 13:54:07 -0700

    deleting a user

--- api/server.js
+++ api/server.js
@@ -5,6 +5,16 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server.delete('/api/users/:id', async (req, res) => {
+    const pickedUser = await User.findById(req.params.id)
+    if (!pickedUser) {
+        res.status(404).json({ message: "The user with the specified ID does not exist" })
+    } else {
+        const deletedUser =  await User.remove(pickedUser.id)
+        res.status(200).json(deletedUser)
+    }
+})
+
 
 server.post('/api/users', (req, res) => {
     const user = req.body;
