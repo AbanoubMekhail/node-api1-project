ac5483d9472f443d8165faf9f2925e8f3c2349d8
abanoub nagib <abanoub.nagib@hotmail.com>
2023-10-19 14:12:41 -0700

    completed mvp

--- api/server.js
+++ api/server.js
@@ -10,62 +10,83 @@ server.delete('/api/users/:id', async (req, res) => {
     if (!pickedUser) {
         res.status(404).json({ message: "The user with the specified ID does not exist" })
     } else {
-        const deletedUser =  await User.remove(pickedUser.id)
+        const deletedUser = await User.remove(pickedUser.id)
         res.status(200).json(deletedUser)
     }
 })
 
+server.put('/api/users/:id', async (req, res) => {
+    try {
+        const pickedUser = await User.findById(req.params.id)
+        if (!pickedUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else {
+            if (!req.body.name || !req.body.bio) {
+                res.status(400).json({ message: "Please provide name and bio for the user" })
+            } else {
+             const updated =  await User.update(req.params.id, req.body)
+             res.status(200).json(updated)
+            }
+        } 
+    }
+        catch (err) {
+            res.status(500).json({
+                message: 'error updating users',
+                err: err.message,
+            })
+        }
+    })
 
 server.post('/api/users', (req, res) => {
     const user = req.body;
     if (!user.name || !user.bio) {
         res.status(400).json({ message: "Please provide name and bio for the user", })
-    }else {
+    } else {
         User.insert(user)
-        .then(newUser => {
-            res.status(201).json(newUser)
-           }).catch(err => {
-            res.status(500).json({
-                message: 'error creating user',
-                err: err.message,
+            .then(newUser => {
+                res.status(201).json(newUser)
+            }).catch(err => {
+                res.status(500).json({
+                    message: 'error creating user',
+                    err: err.message,
+                })
             })
-           })
     }
 })
 
 server.get('/api/users', (req, res) => {
-   User.find()
-   .then(users => {
-    res.json(users)
-   })
-   .catch(err => {
-    res.status(500).json({
-        message: 'error getting users',
-        err: err.message,
-    })
-   })
+    User.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+            })
+        })
 })
 
 
 server.get('/api/users/:id', (req, res) => {
     User.findById(req.params.id)
-    .then(user => {
-        if(!user) {
-            res.status(404).json({ message: "The user with the specified ID does not exist", })
-        }
-    res.json(user)
-    })
-    .catch(err => {
-     res.status(500).json({
-         message: 'error getting users',
-         err: err.message,
-     })
-    })
- })
+        .then(user => {
+            if (!user) {
+                res.status(404).json({ message: "The user with the specified ID does not exist", })
+            }
+            res.json(user)
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'error getting users',
+                err: err.message,
+            })
+        })
+})
 
 
-server.use('*', (req, res)=> {
-    res.status(404).json({message: "not found"})
+server.use('*', (req, res) => {
+    res.status(404).json({ message: "not found" })
 })
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
